bf29f9d1dec711c86e5d29c00874b0574170cde4
iammurad <murad.theynalli@gmail.com>
2022-12-24 12:01:35 +0100

    Relay Command done

--- ModernDesign/Core/RelayCommand.cs
+++ ModernDesign/Core/RelayCommand.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace ModernDesign.Core
@@ -29,4 +25,10 @@ namespace ModernDesign.Core
         {
             return _canExecute == null || _canExecute(parameter);
         }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
 }
