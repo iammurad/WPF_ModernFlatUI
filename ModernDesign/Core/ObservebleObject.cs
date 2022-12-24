f88283aa1bcd43fc05bff38903ca3048f4836fe8
iammurad <murad.theynalli@gmail.com>
2022-12-24 11:59:05 +0100

    Add project files.

---
+++ ModernDesign/Core/ObservebleObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.Core
+{
+    internal class ObservebleObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void onPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
